2c5dbdc6f2d5c2322f0666d70d0db3dd271f7e5d
Marlon Jorge Meireles <marlon.meireles.profissional@gmail.com>
2024-11-14 14:28:54 -0300

    Criptografia de Senha e Hash

--- Modelos/Usuario.js
+++ Modelos/Usuario.js
@@ -1,35 +1,98 @@
+import bcrypt from 'react-native-bcrypt';
+import { NativeModules } from 'react-native';
+import crypto from 'react-native-crypto';
+
+
+const securePRNG = (size) => {
+  return new Promise((resolve, reject) => {
+    NativeModules.RCTDeviceInfo.generateSecureRandomBytes(size, (error, bytes) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(bytes);
+      }
+    });
+  });
+};
+
+crypto.randomBytes = (size, callback) => {
+  securePRNG(size)
+    .then((bytes) => {
+      callback(null, bytes);
+    })
+    .catch((error) => {
+      callback(error);
+    });
+};
+
 class Usuario {
-    #nome;
-    #endereco;
-    #listaEmprestimos;
-    #cpf;
-    #dataNascimento;
+  #nome;
+  #endereco;
+  #listaEmprestimos;
+  #cpf;
+  #dataNascimento;
+  #email;
+  #senhaHash;
 
-    constructor(nome, endereco, CPF, dataNascimento) {
-        this.#nome = nome;
-        this.#endereco = endereco;
-        this.#listaEmprestimos = [];
-        this.#cpf = cpf;
-        this.#dataNascimento = dataNascimento;
-    }
+  async init(nome, endereco, cpf, dataNascimento, email, senha) {
+    this.#nome = nome;
+    this.#endereco = endereco;
+    this.#listaEmprestimos = [];
+    this.#cpf = cpf;
+    this.#dataNascimento = dataNascimento;
+    this.#email = email;
 
-    get getNome() {
-        return this.#nome;
+    try {
+      const salt = await crypto.randomBytes(16).toString('base64');
+      this.#senhaHash = await bcrypt.hashSync(senha, salt);
+    } catch (error) {
+      console.error('Erro ao gerar hash da senha:', error);
+      throw error;
     }
+  }
 
-    get getEndereco() {
-        return this.#endereco;
-    }
+  static async create(nome, endereco, cpf, dataNascimento, email, senha) {
+    const instance = new Usuario();
+    await instance.init(nome, endereco, cpf, dataNascimento, email, senha);
+    return instance;
+  }
 
-    set setEndereco(endereco) {
-        this.#endereco = endereco;
-    }
+  get getNome() {
+    return this.#nome;
+  }
 
-    get getListaEmprestimos() {
-        return this.#listaEmprestimos;
-    }
+  get getEndereco() {
+    return this.#endereco;
+  }
 
-    adicionarEmprestimo(emprestimo) {
-        this.#listaEmprestimos.push(emprestimo);
+  set setEndereco(endereco) {
+    this.#endereco = endereco;
+  }
+
+  get getListaEmprestimos() {
+    return this.#listaEmprestimos;
+  }
+
+  get getemail() {
+    return this.#email;
+  }
+
+  set setemail(email) {
+    this.#email = email;
+  }
+
+  async validarSenha(senha) {
+    try {
+      return await bcrypt.compareSync(senha, this.#senhaHash);
+    } catch (error) {
+      console.error('Erro ao validar senha:', error);
+      throw error;
     }
+  }
+
+  adicionarEmprestimo(emprestimo) {
+    this.#listaEmprestimos.push(emprestimo);
+  }
 }
+
+export default Usuario;
